0fd1bd137f4e6644cdfc434f578d2abcf6cc25b3
aga <rubin.samuel20@yahoo.ro>
2026-02-21 18:27:17 +0200

    Added TeamLimitFix.cs

---
+++ src/TeamLimitFix.cs
@@ -0,0 +1,27 @@
+using SwiftlyS2.Shared.GameEventDefinitions;
+using SwiftlyS2.Shared.GameEvents;
+using SwiftlyS2.Shared.Misc;
+
+namespace Fixes;
+
+public partial class Fixes
+{
+    [GameEventHandler(HookMode.Post)]
+    private HookResult OnRoundStart(EventRoundStart @event)
+    {
+        int maxPlayers = Core.Engine.GlobalVars.MaxClients;
+        int limit = maxPlayers;
+
+        var gameRules = Core.EntitySystem.GetGameRules();
+        if (gameRules != null && gameRules.IsValid)
+        {
+            gameRules.NumSpawnableTerrorist = limit;
+            gameRules.MaxNumTerrorists = limit;
+            
+            gameRules.NumSpawnableCT = limit;
+            gameRules.MaxNumCTs = limit;
+        }
+
+        return HookResult.Continue;
+    }
+}
