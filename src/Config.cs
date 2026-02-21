3e3b15801442253bda6e11eccc2def768c61486c
aga <rubin.samuel20@yahoo.ro>
2026-02-21 18:37:06 +0200

    Added TeamLimitFix.cs (#5)
    * Added TeamLimitFix.cs
    
    * Add config toggle for team limit fix

--- src/Config.cs
+++ src/Config.cs
@@ -4,4 +4,5 @@ public class FixesConfig
 {
     public bool EnableSteamBanFix { get; set; } = true;
     public bool EnableInputActivatorCrashFix { get; set; } = true;
+    public bool EnableTeamLimitFix { get; set; } = true;
 }
